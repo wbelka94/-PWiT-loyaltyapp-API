14dd5638f263ede8388cf97dbc2f363e3106564f
wbelka94 <wbelka94@gmail.com>
2018-06-09 20:14:02 +0200

    filtering companies by customer

--- src/models/CompanySearch.php
+++ src/models/CompanySearch.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Company;
+use yii\db\Query;
 
 /**
  * CompanySearch represents the model behind the search form of `app\models\Company`.
